bab18e5a73c15ac42a0906f6c0c600a7e1b3f42d
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2023-12-19 13:48:04 +0530

    cookies configuration

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -26,7 +26,7 @@ export const attachCookiesToResponse = ({ res, user }) => {
   const oneDay = 1000 * 60 * 60 * 24;
 
   res.cookie("token", token, {
-    httpOnly: false,
+    httpOnly: true,
     sameSite: "none",
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === "production",
